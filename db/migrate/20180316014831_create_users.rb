fe16659a1e424ffac19aac3d2957456eceb6fcf0
Chely Ho <adept_inept@yahoo.com>
2018-03-16 01:50:42 +0000

    automatically backed up by learn

--- db/migrate/20180316014831_create_users.rb
+++ db/migrate/20180316014831_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.password :password
+      t.string :password
     end
   end
 end
