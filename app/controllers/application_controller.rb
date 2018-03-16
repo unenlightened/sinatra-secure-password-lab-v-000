d011f7abcd868dc0d77936e7959924eace1aa0b6
Chely Ho <adept_inept@yahoo.com>
2018-03-16 01:40:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     @user = User.new(username: params[:username], password: params[:password])
-
     redirect @user.save ? '/login' : '/failure'
-
-
   end
 
   get '/account' do
