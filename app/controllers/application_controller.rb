c00e9248084fff1fc4aad251ddff536bac4640d9
Chely Ho <adept_inept@yahoo.com>
2018-03-16 01:56:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     @user = User.new(username: params[:username], password: params[:password])
 
-    redirect '/login'
+    redirect to '/login'
   end
 
   get '/account' do
